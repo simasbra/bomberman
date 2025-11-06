1d6d8b6fdf6ee1bd10bfd91146169dbad8f6ccb4
marsard <martynas.skurdelis@gmail.com>
2025-11-06 08:13:13 +0200

    Bridge pattern
    Change explosion pattern regardless of bomb type.
    TODO: this probably conflicts with AdvancedBomb.cs Execute() function,
    however Execute() is not referenced anywhere so it's unused code.

--- BombermanMultiplayer/Properties/Resources.Designer.cs
+++ BombermanMultiplayer/Properties/Resources.Designer.cs
@@ -163,6 +163,16 @@ namespace BombermanMultiplayer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap explosion_pattern {
+            get {
+                object obj = ResourceManager.GetObject("explosion_pattern", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Fire {
             get {
                 object obj = ResourceManager.GetObject("Fire", resourceCulture);
